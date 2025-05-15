5f798003140b57bb69cfd44d4c9e676df90058af
pieralonso <108495263+pieralonso@users.noreply.github.com>
2025-05-15 21:37:00 +0200

    neotree_remaps//

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -3,17 +3,51 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window
+    -- {"3rd/image.nvim", opts = {}},
   },
-  lazy = false, -- neo-tree will lazily load itself
+  lazy = false,
   opts = {
-    -- puedes agregar aquí opciones de configuración de neo-tree
   },
   config = function()
-    -- Mapeo de tecla para alternar Neo-tree inteligentemente
-    vim.keymap.set('n', '<leader>e', ':Neotree toggle float<CR>', { noremap = true, silent = true })
+
+    local function find_neotree_win()
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        local ft = vim.api.nvim_buf_get_option(buf, "filetype")
+        if ft == "neo-tree" then
+          return win
+        end
+      end
+      return nil
+    end
+
+
+    local function smart_toggle()
+      local neotree_win = find_neotree_win()
+      if neotree_win then
+        if vim.api.nvim_get_current_win() == neotree_win then
+          vim.cmd("wincmd p")
+        else
+          vim.api.nvim_set_current_win(neotree_win)
+        end
+      else
+        vim.cmd("Neotree reveal right")
+      end
+    end
+
+    local function open_close_neotree()
+      local neotree_win = find_neotree_win()
+      if neotree_win then
+        vim.cmd("Neotree close")
+      else
+        vim.cmd("Neotree reveal right")
+      end
+    end
+
+    vim.keymap.set('n', '<leader>e', smart_toggle, { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>E', open_close_neotree, { noremap = true, silent = true })
   end,
 }
 
